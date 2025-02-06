c278c3813d5032920912f463b49983c8e80f274e
drulik44 <sahanzxi984325qx@gmail.com>
2025-02-06 19:32:26 +0200

    remove storyboard

---
+++ PetTranslatorTest/PetTranslatorTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PetTranslatorTest
+//
+//  Created by Руслан Жидких on 06.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+
+
+}
+
