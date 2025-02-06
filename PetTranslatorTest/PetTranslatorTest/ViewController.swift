83da136dec1a0745b10f6d8a6ef842fc2d51de4f
drulik44 <sahanzxi984325qx@gmail.com>
2025-02-06 20:02:48 +0200

    added SnapKit and settings font

--- PetTranslatorTest/PetTranslatorTest/ViewController.swift
+++ PetTranslatorTest/PetTranslatorTest/ViewController.swift
@@ -6,14 +6,28 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .green
+        view.backgroundColor = .red
+
+        let helloLabel: UILabel = {
+            let label = UILabel()
+            label.text = "Hello, World!"
+            label.font = .Konkhmer.Regular.size(of: 20)
+            return label
+        }()
+
+        view.addSubview(helloLabel)
+
+        helloLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview()
+            make.width.equalTo(200)
+            make.height.equalTo(50)
+        }
     }
-
-
 }
-
